1cc41075f1a98ff286390a785478c51f96d3c751
s1l3n0 <g.sileno@uva.nl>
2015-09-06 23:44:57 +0200

    decomposition

--- test/org/leibnizcenter/lppneu/JurixTest.groovy
+++ test/org/leibnizcenter/lppneu/JurixTest.groovy
@@ -3,11 +3,14 @@ package org.leibnizcenter.lppneu
 import org.leibnizcenter.lppneu.components.lppetrinets.LPNet
 import org.leibnizcenter.lppneu.examples.MarketModel
 import org.leibnizcenter.pneu.animation.monolithic.NetRunner
+import org.leibnizcenter.pneu.builders.PN2LaTeX
 import org.leibnizcenter.pneu.components.petrinet.Net
 import org.leibnizcenter.pneu.components.petrinet.Place
 import org.leibnizcenter.pneu.components.petrinet.Transition
+import org.leibnizcenter.pneu.parsers.PNML2PN
 
 class JurixTest extends GroovyTestCase {
+
 
     void testGroundNetsExports() {
 
@@ -51,4 +54,41 @@ class JurixTest extends GroovyTestCase {
         runner.analysis.exportToLog("groundSaleNormativeModel")
 
     }
+
+    void testDecomposition1() {
+
+        Net model = MarketModel.groundSaleInstance()
+
+        NetRunner runner = new NetRunner()
+        runner.load(model)
+        runner.analyse()
+
+        D
+
+    }
+
+    void testDecomposition2() {
+
+        Net model = MarketModel.groundSaleModel()
+
+        NetRunner runner = new NetRunner()
+        runner.load(model)
+        runner.analyse()
+        runner.status()
+
+    }
+
+    void testDecomposition3() {
+
+        Net model = MarketModel.groundSaleNormativeModel()
+
+        NetRunner runner = new NetRunner()
+        runner.load(model)
+        runner.analyse()
+        runner.status()
+
+        runner.analysis.exportToLog("groundSaleNormativeModel")
+
+    }
+
 }
